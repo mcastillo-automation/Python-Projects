67edc9d741ec7f5c36af16cacfa3ef9d1809e03b
CMALuca <72840604+CMALuca@users.noreply.github.com>
2022-11-30 22:38:27 -0800

    Updated mode for line 15. Didn't need to be w+. w would suffice.

--- File Writing/Mail Merge/main.py
+++ File Writing/Mail Merge/main.py
@@ -12,5 +12,5 @@ with open('./Input/Letters/starting_letter.txt', mode='r') as file:
 
 # Iterate through names list and use re.sub() to replace [names] with list values.
 for items in names:
-    with open(f'./Output/ReadyToSend/{items}.txt', mode='w+') as text:
+    with open(f'./Output/ReadyToSend/{items}.txt', mode='w') as text:
         text.write(re.sub(pattern, repl=items, string=template))
