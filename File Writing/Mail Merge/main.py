38b34ac7b849942614acae4a21d43b697f3997ad
CMALuca <72840604+CMALuca@users.noreply.github.com>
2022-11-30 22:27:07 -0800

    Created mail merge project

---
+++ File Writing/Mail Merge/main.py
@@ -0,0 +1,16 @@
+import re
+
+pattern = "\Wname\W"
+
+# Grab invited_names.txt and store lines as a list.
+with open('./Names/invited_names.txt', mode='r') as file:
+    names = file.read().splitlines()
+
+# Read starting_letter.txt and store it as template
+with open('./Input/Letters/starting_letter.txt', mode='r') as file:
+    template = file.read()
+
+# Iterate through names list and use re.sub() to replace [names] with list values.
+for items in names:
+    with open(f'./Output/ReadyToSend/{items}.txt', mode='w+') as text:
+        text.write(re.sub(pattern, items, template))
