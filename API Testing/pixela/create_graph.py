f05a212fc18ebb64430a44d8d37453f76874c46a
CMALuca <72840604+CMALuca@users.noreply.github.com>
2022-12-21 21:28:48 -0800

    pixela testing

---
+++ API Testing/pixela/create_graph.py
@@ -0,0 +1,23 @@
+import requests
+import config
+
+'''
+Messing around with the Pixela api. Available here: https://pixe.la/ This creates a graph.
+'''
+
+ENDPOINT = 'https://pixe.la/v1/users'
+graph_endpoint = f'{ENDPOINT}/{config.USERNAME}/graphs'
+
+headers = {
+    'X-USER-TOKEN': config.TOKEN
+}
+request_body = {
+    'id': config.GRAPH_ID,
+    'name': 'Coding Practice',
+    'unit': 'commit',
+    'type': 'int',
+    'color': 'shibafu'
+}
+
+response = requests.post(url=graph_endpoint, headers=headers, json=request_body)
+print(response.text)
---
+++ API Testing/pixela/update_graph.py
@@ -0,0 +1,23 @@
+import requests
+import datetime
+import config
+
+
+'''Messing around with the Pixela api. Available here: https://pixe.la/ This allows a user to add a dot to their 
+pixela graph. '''
+
+ENDPOINT = 'https://pixe.la/v1/users'
+graph_endpoint = f'{ENDPOINT}/{config.USERNAME}/graphs/{config.GRAPH_ID}'
+current_day = datetime.date.today().strftime('%Y%m%d')
+
+
+headers = {
+    'X-USER-TOKEN': config.TOKEN
+}
+request_body = {
+    'date': current_day,
+    'quantity': '1'
+}
+
+response = requests.post(url=graph_endpoint, headers=headers, json=request_body)
+print(response.text)
